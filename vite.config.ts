230ec70dc33e682ba2ea9c6ee3bc7cbec91cf0ae
OnoderaTakuma <25942571+onodera0114@users.noreply.github.com>
2024-09-06 22:08:41 +0900

    huskyとvitestを導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig as defineVitestConfig } from "vitest/config";
 
-// https://vitejs.dev/config/
+const vitestConfig = defineVitestConfig({
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./vitest.setup.ts",
+  },
+});
+
 export default defineConfig({
   plugins: [react()],
+  ...vitestConfig,
 });
---
+++ vitest.setup.ts
@@ -0,0 +1,9 @@
+import { cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+
+export const user = userEvent.setup();
+
+afterEach(() => {
+  cleanup();
+});
