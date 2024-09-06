5fe3d16aaddc1d8f069041cc20da265ec6274b21
OnoderaTakuma <25942571+onodera0114@users.noreply.github.com>
2024-09-07 06:30:42 +0900

    dotenvをアンインストール

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig as defineVitestConfig } from "vitest/config";
-import dotenv from "dotenv";
-
 
 const vitestConfig = defineVitestConfig({
   test: {
     environment: "happy-dom",
     globals: true,
     setupFiles: "./vitest.setup.ts",
-    env: dotenv.config({ path: ".env.test.local" }).parsed,
   },
 });
 
