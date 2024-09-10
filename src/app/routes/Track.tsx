51d65334faec51712f69b6826b965d9ddd5c801e
OnoderaTakuma <25942571+onodera0114@users.noreply.github.com>
2024-09-11 01:51:17 +0900

    曲の取得処理を実装

--- src/app/routes/Track.tsx
+++ src/app/routes/Track.tsx
@@ -1,7 +1,47 @@
-export const Track = (): JSX.Element => {
+import { FetchQueryOptions, QueryClient, QueryKey } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
+import { useParams, LoaderFunctionArgs } from "react-router-dom";
+
+import { getTrackQueryOptions, useTrack } from "@/features/track/api/getTrack";
+import { TrackResponse } from "@/types/track";
+
+export const trackLoader =
+  (queryClient: QueryClient) =>
+    async ({ params }: LoaderFunctionArgs): Promise<TrackResponse | unknown> => {
+      const trackId = params.trackId as string;
+
+      const trackQuery = getTrackQueryOptions(trackId);
+
+      return queryClient.getQueryData(trackQuery.queryKey as QueryKey) ?? (await queryClient.fetchQuery(trackQuery as FetchQueryOptions));
+    };
+
+export const Track = (): JSX.Element | null => {
+  const params = useParams();
+  const trackId = params.trackId as string;
+  const trackQuery = useTrack({
+    trackId,
+  });
+
+  if (trackQuery.isLoading) {
+    return (
+      <div>
+        <p>ローディング</p>
+      </div>
+    );
+  }
+
+  const track = trackQuery.data;
+
+  if (!track) return null;
+
   return (
     <>
-      <h1>曲詳細</h1>
+      <div>
+        <ErrorBoundary fallback={<div>Failed to load comments. Try to refresh the page.</div>}>
+          <div>曲詳細</div>
+          <div>{JSON.stringify(track)}</div>
+        </ErrorBoundary>
+      </div>
     </>
   );
 };
