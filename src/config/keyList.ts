bae7312b27e3b8705e6fd52cd598cb40ac95e6be
OnoderaTakuma <25942571+onodera0114@users.noreply.github.com>
2024-09-19 21:53:33 +0900

    キー一覧を修正

--- src/config/keyList.ts
+++ src/config/keyList.ts
@@ -12,9 +12,21 @@ export const trackKeyList: ListItems[] = [
     descriptionText: "",
   },
   {
-    key: "album",
-    teamText: "アルバム",
+    key: "release_date",
+    teamText: "リリース日",
     descriptionText: "",
+  },
+  {
+    key: "album_name",
+    teamText: "アルバム名",
+    descriptionText: "",
+    noteText: "リンクをクリックでシングル・アルバムの詳細ページに遷移します。",
+  },
+  {
+    key: "album_type",
+    teamText: "種類",
+    descriptionText: "",
+    noteText: "album、single、compilationのどれかの値をとります",
   },
   {
     key: "artists",
@@ -50,8 +62,25 @@ export const trackKeyList: ListItems[] = [
     descriptionText: "",
   },
   {
-    key: "external_ids",
-    teamText: "external_ids",
+    key: "isrc",
+    teamText: "ISRC",
     descriptionText: "",
+    noteTitle: "ISRCとは？",
+    noteText:
+      "「International Standard Recording Code」の略称で、日本語では「国際標準レコーディングコード」といいます。\nレコーディング（オーディオレコーディング／音楽ビデオレコーディング）の識別に利用される唯一の国際標準コードで、1つのレコーディングは1つのISRCで識別されます。\n一度付番したISRCを変更することはできません。",
+  },
+  {
+    key: "ean",
+    teamText: "EAN",
+    descriptionText: "",
+    noteTitle: "EANとは？",
+    noteText: "「European article number」の略称で、「イアンコード」と読みます。\nヨーロッパで統一されている13桁で構成される商品コードのことです。",
+  },
+  {
+    key: "upc",
+    teamText: "UPC",
+    descriptionText: "",
+    noteTitle: "UPCとは？",
+    noteText: "「Universal Product Code」の略称で、カナダ、アメリカで統一されている商品コードのことです。",
   },
 ];
