c0ad391f5ef79800d1f992940e02f6adee9cbe57
OnoderaTakuma <25942571+onodera0114@users.noreply.github.com>
2024-09-26 01:33:14 +0900

    パンくずリストを設置

--- src/app/routes/Root.tsx
+++ src/app/routes/Root.tsx
@@ -1,12 +1,17 @@
 import { Header } from "@/components/layouts/Header";
+import { Breadcrumbs } from "@/components/ui/navigations/Breadcrumbs";
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import { useBreadcrumbs } from "@/hooks/useBreadcrumbs";
 
 export const Root = (): JSX.Element => {
+  const { breadcrumbItem } = useBreadcrumbs();
+
   return (
     <>
       <Header />
       <Box component="main" sx={{ p: 2 }}>
+        <Breadcrumbs breadcrumbItem={breadcrumbItem} />
         <Outlet />
       </Box>
     </>
