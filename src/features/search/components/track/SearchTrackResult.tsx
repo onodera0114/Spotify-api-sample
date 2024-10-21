871e663897c1bfd6968e80fd92f3be9d94dcd605
OnoderaTakuma <25942571+onodera0114@users.noreply.github.com>
2024-10-21 12:51:35 +0900

    クリック時の遷移処理を実装

--- src/features/search/components/track/SearchTrackResult.tsx
+++ src/features/search/components/track/SearchTrackResult.tsx
@@ -1,26 +1,29 @@
 import { TrackResponse } from "@/types/track";
 import { convertMs } from "@/utils/convertMs";
 import { Box, List, ListItem, ListItemButton, Stack, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   trackResult: TrackResponse[];
 };
 
 export const SearchTrackResult: React.FC<Props> = (props: Props): JSX.Element => {
+  const navigate = useNavigate();
   const { trackResult } = props;
+
   return (
     <Box sx={{ mt: 4 }}>
       <List sx={{ width: "100%", bgcolor: "background.paper" }} component="ul">
         {trackResult.map((track) => (
-          <ListItem key={track.id} disablePadding>
-            <ListItemButton>
+          <ListItem key={track.id} component="li" disablePadding>
+            <ListItemButton onClick={() => navigate(`/track/${track.id}`)}>
               <Stack spacing={2} direction="row" sx={{ flexGrow: 1, alignItems: "center" }}>
-                <div style={{ textAlign: "center" }}>
+                <Box style={{ textAlign: "center" }}>
                   <img src={track.album.images[0].url} alt="" style={{ width: "64px", height: "64px" }} />
-                </div>
+                </Box>
                 <Stack sx={{ flexGrow: 1, textAlign: "left" }}>
-                  <Typography>{track.name}</Typography>
-                  <Typography>{track.artists.map((artist) => artist.name).join(", ")}</Typography>
+                  <Typography variant="body1">{track.name}</Typography>
+                  <Typography variant="body2">{track.artists.map((artist) => artist.name).join(", ")}</Typography>
                 </Stack>
                 <Typography>{`${convertMs(track.duration_ms).minutes}分${convertMs(track.duration_ms).seconds}秒`}</Typography>
               </Stack>
