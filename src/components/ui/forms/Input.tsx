82e31a866c372640380b199bf607a627e371f0ec
OnoderaTakuma <25942571+onodera0114@users.noreply.github.com>
2024-10-21 12:45:22 +0900

    入力コンポーネントにchangeイベントを追加

--- src/components/ui/forms/Input.tsx
+++ src/components/ui/forms/Input.tsx
@@ -4,9 +4,10 @@ type Props = {
   label: string;
   variant?: "outlined" | "filled" | "standard";
   fullWidth?: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 export const Input: React.FC<Props> = (props: Props): JSX.Element => {
-  const { label, variant = "outlined", fullWidth = true } = props;
-  return <TextField label={label} variant={variant} fullWidth={fullWidth} />;
+  const { label, variant = "outlined", fullWidth = true, onChange } = props;
+  return <TextField label={label} variant={variant} fullWidth={fullWidth} onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e)} />;
 };
