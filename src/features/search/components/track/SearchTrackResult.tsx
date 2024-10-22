3b6164ebbd4f951ff56733805a7b29c7f857442f
OnoderaTakuma <25942571+onodera0114@users.noreply.github.com>
2024-10-22 13:15:55 +0900

    margin修正

--- src/features/search/components/track/SearchTrackResult.tsx
+++ src/features/search/components/track/SearchTrackResult.tsx
@@ -14,25 +14,23 @@ export const SearchTrackResult: React.FC<Props> = (props: Props): JSX.Element =>
   const { trackResult } = props;
 
   return (
-    <Box sx={{ mt: 4 }}>
-      <List sx={{ width: "100%", bgcolor: "background.paper" }} component="ul">
-        {trackResult.map((track) => (
-          <ListItem key={track.id} component="li" disablePadding>
-            <ListItemButton onClick={() => navigate(`/track/${track.id}`)}>
-              <Stack spacing={2} direction="row" sx={{ flexGrow: 1, alignItems: "center" }}>
-                <Box style={{ textAlign: "center" }}>
-                  <img src={track.album.images[0].url} alt="" style={{ width: "64px", height: "64px" }} />
-                </Box>
-                <Stack sx={{ flexGrow: 1, textAlign: "left" }}>
-                  <Typography variant="body1">{track.name}</Typography>
-                  <Typography variant="body2">{track.artists.map((artist) => artist.name).join(", ")}</Typography>
-                </Stack>
-                {matches && <Typography>{`${convertMs(track.duration_ms).minutes}分${convertMs(track.duration_ms).seconds}秒`}</Typography>}
+    <List sx={{ width: "100%", bgcolor: "background.paper", p: 0 }} component="ul">
+      {trackResult.map((track) => (
+        <ListItem key={track.id} component="li" disablePadding>
+          <ListItemButton onClick={() => navigate(`/track/${track.id}`)}>
+            <Stack spacing={2} direction="row" sx={{ flexGrow: 1, alignItems: "center" }}>
+              <Box style={{ textAlign: "center" }}>
+                <img src={track.album.images[0].url} alt="" style={{ width: "64px", height: "64px" }} />
+              </Box>
+              <Stack sx={{ flexGrow: 1, textAlign: "left" }}>
+                <Typography variant="body1">{track.name}</Typography>
+                <Typography variant="body2">{track.artists.map((artist) => artist.name).join(", ")}</Typography>
               </Stack>
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
+              {matches && <Typography>{`${convertMs(track.duration_ms).minutes}分${convertMs(track.duration_ms).seconds}秒`}</Typography>}
+            </Stack>
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
   );
 };
